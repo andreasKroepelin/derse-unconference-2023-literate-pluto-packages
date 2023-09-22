4a5d1482c0d66b81568d742f396422ffafb4e339
Wolfhart Feldmeier <wolfhart.feldmeier@uni-jena.de>
2023-09-22 15:17:54 +0200

    tests for ExamplePackage

--- ExamplePackage/test/runtests.jl
+++ ExamplePackage/test/runtests.jl
@@ -2,5 +2,14 @@ using ExamplePackage
 using Test
 
 @testset "ExamplePackage.jl" begin
-    # Write your tests here.
+    @testset "dot" begin
+        e_x = [1, 0, 0]
+        e_y = [0, 1, 0]
+        e_z = [0, 0, 1]
+        
+        @test dot(e_x, e_x) == 1
+        @test dot(e_x, 5 .* e_x) == 5
+        @test dot(e_x, e_y) == 0
+        @test dot(e_x, e_z) == 0
+    end
 end
